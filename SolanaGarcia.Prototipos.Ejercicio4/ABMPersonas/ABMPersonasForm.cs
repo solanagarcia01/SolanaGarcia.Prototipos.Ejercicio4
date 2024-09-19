7ffa2d2487b11153f9a0200c67c66cf3219038a0
solana garcia <solanagarcia01@gmail.com>
2024-09-19 19:56:53 -0300

    Agregar archivos de proyecto.

---
+++ SolanaGarcia.Prototipos.Ejercicio4/ABMPersonas/ABMPersonasForm.cs
@@ -0,0 +1,25 @@
+namespace SolanaGarcia.Prototipos.Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SolanaGarcia.Prototipos.Ejercicio4/ABMPersonas/ABMPersonasModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolanaGarcia.Prototipos.Ejercicio4.ABMPersonas
+{
+    internal class ABMPersonasModel
+    {
+    }
+}
